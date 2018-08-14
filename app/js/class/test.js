48b642c171e2b2c4fb68fbfd97eb291491ca23e0
DZHdzh692817 <1120951042@qq.com>
2018-08-14 16:20:27 +0800

    es6语法

--- app/js/class/test.js
+++ app/js/class/test.js
@@ -54,3 +54,196 @@ console.table([
     }
     console.log("foo()===1",foo()===1);
 }
+
+//箭头函数
+//es5
+/*function aa() {}*/
+//es6
+/*()=>{}*/
+{
+    //es5
+    var evens = [1,2,3,4,5];
+    var odds = evens.map(function(v){//遍历
+        return v + 1;
+    })
+    console.log(evens,odds);
+}
+
+{
+    //es6
+    let evens = [1,2,3,4,5];
+    let odds = evens.map(v => v+1);
+    console.log(evens,odds);
+}
+
+{//es3 es5写法
+    var factory = function () {
+        this.a = 'a';
+        this.b = 'b';
+        this.c={
+            a:'a+',
+            b:function () {
+                return this.a;
+            }
+        }
+    }
+    console.log(new factory().c.b());//输出a+
+}
+
+{//es6写法
+    let factory = function () {
+        this.a = 'a';
+        this.b = 'b';
+        this.c={
+            a:'a+',
+            b:()=>{//箭头函数
+                return this.a;
+            }
+        }
+    }
+    console.log(new factory().c.b());//输出a
+}
+
+//默认参数
+{
+    //es3/es5默认参数写法
+    function f(x, y, z) {
+        if(y === undefined){
+            y = 7;
+        }
+        if(z === undefined){
+            z = 42;
+        }
+        return x + y + z;
+    }
+    console.log(f(1,3));
+}
+
+{
+    //es6默认参数写法
+    function ffs(x, y = 7, z = 42) {
+        return x + y + z;
+    }
+    console.log(ffs(1,3));
+}
+
+{
+    //es3 es5可变参数
+    function fx() {
+        //console.log(x);
+        //求和运算
+        var a = Array.prototype.slice.call(arguments);
+        var sum = 0;
+        a.forEach(function (item) {
+            sum += item * 1;
+        })
+        return sum;
+    }
+    console.log(fx(1,2,3));
+}
+
+{
+    //es6可变参数
+    function  fxo(...a) {
+        let sum = 0;
+        a.forEach(item=>{
+            sum += item * 1;
+        })
+        return sum;
+    }
+    console.log(fxo(1,2,3,6));
+}
+
+{
+    //es5合并数组
+    var params = ['hello', true, 7];
+    var other = [1,2].concat(params);
+    console.log(other);
+}
+
+{
+    //es6 利用扩展运算符合并数组
+    var params = ['hello', true, 7];
+    var other = [1,2,...params];
+    console.log(other);
+}
+
+//对象代理
+{
+    //es3 es5数据保护
+    var Person = function () {
+        var data = {
+            name:'es3',
+            sex:'male',
+            age:15
+        }
+        this.get = function (key) {
+            return data[key];
+        }
+        this.set = function (key,value) {
+            if(key !== 'sex'){
+                data[key] = value;
+            }
+        }
+    }
+    //声明一个实例
+    var person = new Person();
+    //读取
+    console.table({name:person.get('name'), sex:person.get('sex'), age:person.get('age')});
+    //修改
+    person.set('name','es3-cname');
+    console.table({name:person.get('name'), sex:person.get('sex'), age:person.get('age')});
+
+    person.set('sex','female');
+    console.table({name:person.get('name'), sex:person.get('sex'), age:person.get('age')});
+}
+
+{
+    //es5数据保护
+    var Person = {
+        name : 'es5',
+        age : 15
+    };
+    Object.defineProperty(Person,'sex',{
+       writable:false,
+       value:'male'
+    });
+    console.table({name:Person.name,age:Person.age,sex:Person.sex});
+
+    Person.name = 'es5-cname';
+    console.table({name:Person.name,age:Person.age,sex:Person.sex});
+
+    //报错了 Cannot assign to read only property 'sex' of object '#<Object>'
+    //Person.sex = 'female';
+    //console.table({name:Person.name,age:Person.age,sex:Person.sex});
+
+}
+
+{
+    //es6数据保护
+    let Person = {
+        name : 'es6',
+        sex  : 'male',
+        age  : 15
+    };
+    //代理
+    let person = new Proxy(Person,{
+        get(target, key){
+            return target[key];
+        },
+        set(target,key,value){
+            if(key !== 'sex'){
+                target[key] = value;
+            }
+        }
+    });
+    console.table({name:person.name,age:person.age,sex:person.sex});
+    //报错了 person.sex不允许被修改
+    /*try {
+        person.sex='female';
+    }catch (e) {
+        console.log(e);
+    }finally {
+
+    }*/
+}
--- server/public/js/index.js
+++ server/public/js/index.js
@@ -137,5 +137,216 @@
 	    console.log("foo()===1", foo() === 1);
 	}
 
+	//箭头函数
+	//es5
+	/*function aa() {}*/
+	//es6
+	/*()=>{}*/
+	{
+	    //es5
+	    var evens = [1, 2, 3, 4, 5];
+	    var odds = evens.map(function (v) {
+	        //遍历
+	        return v + 1;
+	    });
+	    console.log(evens, odds);
+	}
+
+	{
+	    //es6
+	    var _evens = [1, 2, 3, 4, 5];
+	    var _odds = _evens.map(function (v) {
+	        return v + 1;
+	    });
+	    console.log(_evens, _odds);
+	}
+
+	{
+	    //es3 es5写法
+	    var factory = function factory() {
+	        this.a = 'a';
+	        this.b = 'b';
+	        this.c = {
+	            a: 'a+',
+	            b: function b() {
+	                return this.a;
+	            }
+	        };
+	    };
+	    console.log(new factory().c.b()); //输出a+
+	}
+
+	{
+	    //es6写法
+	    var _factory = function _factory() {
+	        var _this = this;
+
+	        this.a = 'a';
+	        this.b = 'b';
+	        this.c = {
+	            a: 'a+',
+	            b: function b() {
+	                //箭头函数
+	                return _this.a;
+	            }
+	        };
+	    };
+	    console.log(new _factory().c.b()); //输出a
+	}
+
+	//默认参数
+	{
+	    //es3/es5默认参数写法
+	    var f = function f(x, y, z) {
+	        if (y === undefined) {
+	            y = 7;
+	        }
+	        if (z === undefined) {
+	            z = 42;
+	        }
+	        return x + y + z;
+	    };
+
+	    console.log(f(1, 3));
+	}
+
+	{
+	    //es6默认参数写法
+	    var ffs = function ffs(x) {
+	        var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 7;
+	        var z = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 42;
+
+	        return x + y + z;
+	    };
+
+	    console.log(ffs(1, 3));
+	}
+
+	{
+	    //es3 es5可变参数
+	    var fx = function fx() {
+	        //console.log(x);
+	        //求和运算
+	        var a = Array.prototype.slice.call(arguments);
+	        var sum = 0;
+	        a.forEach(function (item) {
+	            sum += item * 1;
+	        });
+	        return sum;
+	    };
+
+	    console.log(fx(1, 2, 3));
+	}
+
+	{
+	    //es6可变参数
+	    var fxo = function fxo() {
+	        var sum = 0;
+
+	        for (var _len = arguments.length, a = Array(_len), _key = 0; _key < _len; _key++) {
+	            a[_key] = arguments[_key];
+	        }
+
+	        a.forEach(function (item) {
+	            sum += item * 1;
+	        });
+	        return sum;
+	    };
+
+	    console.log(fxo(1, 2, 3, 6));
+	}
+
+	{
+	    //es5合并数组
+	    var params = ['hello', true, 7];
+	    var other = [1, 2].concat(params);
+	    console.log(other);
+	}
+
+	{
+	    //es6 利用扩展运算符合并数组
+	    var params = ['hello', true, 7];
+	    var other = [1, 2].concat(params);
+	    console.log(other);
+	}
+
+	//对象代理
+	{
+	    //es3 es5数据保护
+	    var Person = function Person() {
+	        var data = {
+	            name: 'es3',
+	            sex: 'male',
+	            age: 15
+	        };
+	        this.get = function (key) {
+	            return data[key];
+	        };
+	        this.set = function (key, value) {
+	            if (key !== 'sex') {
+	                data[key] = value;
+	            }
+	        };
+	    };
+	    //声明一个实例
+	    var person = new Person();
+	    //读取
+	    console.table({ name: person.get('name'), sex: person.get('sex'), age: person.get('age') });
+	    //修改
+	    person.set('name', 'es3-cname');
+	    console.table({ name: person.get('name'), sex: person.get('sex'), age: person.get('age') });
+
+	    person.set('sex', 'female');
+	    console.table({ name: person.get('name'), sex: person.get('sex'), age: person.get('age') });
+	}
+
+	{
+	    //es5数据保护
+	    var Person = {
+	        name: 'es5',
+	        age: 15
+	    };
+	    Object.defineProperty(Person, 'sex', {
+	        writable: false,
+	        value: 'male'
+	    });
+	    console.table({ name: Person.name, age: Person.age, sex: Person.sex });
+
+	    Person.name = 'es5-cname';
+	    console.table({ name: Person.name, age: Person.age, sex: Person.sex });
+
+	    //报错了 Cannot assign to read only property 'sex' of object '#<Object>'
+	    //Person.sex = 'female';
+	    //console.table({name:Person.name,age:Person.age,sex:Person.sex});
+	}
+
+	{
+	    //es6数据保护
+	    var _Person = {
+	        name: 'es6',
+	        sex: 'male',
+	        age: 15
+	    };
+	    //代理
+	    var _person = new Proxy(_Person, {
+	        get: function get(target, key) {
+	            return target[key];
+	        },
+	        set: function set(target, key, value) {
+	            if (key !== 'sex') {
+	                target[key] = value;
+	            }
+	        }
+	    });
+	    console.table({ name: _person.name, age: _person.age, sex: _person.sex });
+	    //报错了 person.sex不允许被修改
+	    /*try {
+	        person.sex='female';
+	    }catch (e) {
+	        console.log(e);
+	    }finally {
+	      }*/
+	}
+
 /***/ })
 /******/ ]);
