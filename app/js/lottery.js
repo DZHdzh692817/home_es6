b2fb5edbf6610368bff3cc3e2e37bc69f0655cb2
DZHdzh692817 <1120951042@qq.com>
2018-08-18 19:11:55 +0800

    更新lottery

--- app/js/lottery.js
+++ app/js/lottery.js
@@ -4,3 +4,92 @@ import Timer from './lottery/timer.js';
 import Calculate from './lottery/calculate.js';
 import Interface from './lottery/interface.js';
 import $ from 'jquery';
+
+//深度拷贝
+const copyProperties=function(target,source){
+    for(let key of Reflect.ownKeys(source)){
+        if(key!=='constructor'&&key!=='prototype'&&key!=='name'){
+            let desc=Object.getOwnPropertyDescriptor(source,key);
+            Object.defineProperty(target,key,desc);
+        }
+    }
+}
+
+//多重继承
+const mix=function(...mixins){
+    class Mix{}
+    for(let mixin of mixins){
+        copyProperties(Mix,mixin);
+        copyProperties(Mix.prototype,mixin.prototype);
+    }
+    return Mix
+}
+
+class Lottery extends mix(Base,Calculate,Interface,Timer){
+    //构造函数
+    constructor(name='syy',cname='11选5',issue='**',state='**'){
+        super();//放在最前面
+        this.name=name;//
+        this.cname=cname;//
+        this.issue=issue;//
+        this.state=state;//
+        this.el='';//当前选择器
+        this.omit=new Map();//遗漏
+        this.open_code=new Set();//开奖号码
+        this.open_code_list=new Set();//开奖记录
+        this.play_list=new Map();//玩法列表
+        this.number=new Set();//选号
+        this.issue_el='#curr_issue';//当前选号 期号
+        this.countdown_el='#countdown';//倒计时
+        this.state_el='.state_el';//状态
+        this.cart_el='.codelist';//购物车
+        this.omit_el='';//遗漏
+        this.cur_play='r5';//默认玩法
+        this.initPlayList();//
+        this.initNumber();//
+        this.updateState();//更新状态
+        this.initEvent();//事件初始化
+    }
+
+    /**
+     * [updateState 状态更新]
+     * @return {[type]} [description]
+     */
+    updateState(){
+        let self=this;
+        this.getState().then(function(res){
+            self.issue=res.issue;//当前期号
+            self.end_time=res.end_time;//更新时间
+            self.state=res.state;//状态
+            $(self.issue_el).text(res.issue);
+            self.countdown(res.end_time,function(time){
+                $(self.countdown_el).html(time)//倒计时
+            },function(){
+                setTimeout(function () {
+                    self.updateState();
+                    self.getOmit(self.issue).then(function(res){
+
+                    });
+                    self.getOpenCode(self.issue).then(function(res){
+
+                    })
+                }, 500);//500毫秒
+            })
+        })
+    }
+
+    /**
+     * [initEvent 初始化事件]
+     * @return {[type]} [description]
+     */
+    initEvent(){
+        let self=this;
+        $('#plays').on('click','li',self.changePlayNav.bind(self));
+        $('.boll-list').on('click','.btn-boll',self.toggleCodeActive.bind(self));
+        $('#confirm_sel_code').on('click',self.addCode.bind(self));
+        $('.dxjo').on('click','li',self.assistHandle.bind(self));
+        $('.qkmethod').on('click','.btn-middle',self.getRandomCode.bind(self));
+    }
+}
+
+export default Lottery;
--- app/js/lottery/base.js
+++ app/js/lottery/base.js
@@ -5,49 +5,52 @@ class Base{
      * @return {[type]} [description]
      */
     initPlayList(){
+        //ES6提供了数据结构Set。类似于数组，但是没有重复值
         this.play_list.set('r2',{
             bonus:6,
             tip:'从01～11中任选2个或多个号码，所选号码与开奖号码任意两个号码相同，即中奖<em class="red">6</em>元',
             name:'任二'
         })
-            .set('r3',{
-                bonus:19,
-                tip:'从01～11中任选3个或多个号码，选号与奖号任意三个号相同，即中奖<em class="red">19</em>元',
-                name:'任三'
-            })
-            .set('r4',{
-                bonus:78,
-                tip:'从01～11中任选4个或多个号码，所选号码与开奖号码任意四个号码相同，即中奖<em class="red">78</em>元',
-                name:'任四'
-            })
-            .set('r5',{
-                bonus:540,
-                tip:'从01～11中任选5个或多个号码，所选号码与开奖号码相同，即中奖<em class="red">540</em>元',
-                name:'任五'
-            })
-            .set('r6',{
-                bonus:90,
-                tip:'从01～11中任选6个或多个号码，所选号码与开奖号码五个号码相同，即中奖<em class="red">90</em>元',
-                name:'任六'
-            })
-            .set('r7',{
-                bonus:26,
-                tip:'从01～11中任选7个或多个号码，选号与奖号五个号相同，即中奖<em class="red">26</em>元',
-                name:'任七'
-            })
-            .set('r8',{
-                bonus:9,
-                tip:'从01～11中任选8个或多个号码，选号与奖号五个号相同，即中奖<em class="red">9</em>元',
-                name:'任八'
-            })
+        .set('r3',{
+            bonus:19,
+            tip:'从01～11中任选3个或多个号码，选号与奖号任意三个号相同，即中奖<em class="red">19</em>元',
+            name:'任三'
+        })
+        .set('r4',{
+            bonus:78,
+            tip:'从01～11中任选4个或多个号码，所选号码与开奖号码任意四个号码相同，即中奖<em class="red">78</em>元',
+            name:'任四'
+        })
+        .set('r5',{
+            bonus:540,
+            tip:'从01～11中任选5个或多个号码，所选号码与开奖号码相同，即中奖<em class="red">540</em>元',
+            name:'任五'
+        })
+        .set('r6',{
+            bonus:90,
+            tip:'从01～11中任选6个或多个号码，所选号码与开奖号码五个号码相同，即中奖<em class="red">90</em>元',
+            name:'任六'
+        })
+        .set('r7',{
+            bonus:26,
+            tip:'从01～11中任选7个或多个号码，选号与奖号五个号相同，即中奖<em class="red">26</em>元',
+            name:'任七'
+        })
+        .set('r8',{
+            bonus:9,
+            tip:'从01～11中任选8个或多个号码，选号与奖号五个号相同，即中奖<em class="red">9</em>元',
+            name:'任八'
+        })
     }
     /**
      * [initNumber 初始化号码]
      * @return {[type]} [description]
      */
     initNumber(){
-        for(let i=1;i<12;i++){
-            this.number.add((''+i).padStart(2,'0'))
+        for(let i=1;i<12;i++){//应该遍历1到11 1变成01 2变成02...
+            //String.prototype.padStart(maxLength, fillString=’ ‘)
+            //该方法是在字符串前使用 fillString 填充，直到字符串长度为 maxLength ：
+            this.number.add((''+i).padStart(2,'0'));//填充字符串，长度为2 在字符前面加0 比如 01 02 03...
         }
     }
 
@@ -56,7 +59,7 @@ class Base{
      * @param {[type]} omit [description]
      */
     setOmit(omit){
-        let self=this;
+        let self=this;//指代本作用域的对象
         self.omit.clear();
         for(let [index,item] of omit.entries()){
             self.omit.set(index,item)
@@ -71,7 +74,7 @@ class Base{
      * @param {[type]} code [description]
      */
     setOpenCode(code){
-        let self=this;
+        let self=this;//指代本作用域的对象
         self.open_code.clear();
         for(let item of code.values()){
             self.open_code.add(item);
@@ -85,9 +88,9 @@ class Base{
      * @return {[type]}   [description]
      */
     toggleCodeActive(e){
-        let self=this;
-        let $cur=$(e.currentTarget);
-        $cur.toggleClass('btn-boll-active');
+        let self=this;//指代本作用域的对象
+        let $cur=$(e.currentTarget);//currentTarget 事件属性返回其监听器触发事件的节点
+        $cur.toggleClass('btn-boll-active');//toggleClass() 对设置或移除被选元素的一个或多个类进行切换。
         self.getCount();
     }
 
@@ -97,51 +100,51 @@ class Base{
      * @return {[type]}   [description]
      */
     changePlayNav(e){
-        let self=this;
-        let $cur=$(e.currentTarget);
+        let self=this;//指代本作用域的对象
+        let $cur=$(e.currentTarget);//currentTarget 事件属性返回其监听器触发事件的节点
         $cur.addClass('active').siblings().removeClass('active');
-        self.cur_play=$cur.attr('desc').toLocaleLowerCase();
+        self.cur_play=$cur.attr('desc').toLocaleLowerCase();//小写
         $('#zx_sm span').html(self.play_list.get(self.cur_play).tip);
         $('.boll-list .btn-boll').removeClass('btn-boll-active');
         self.getCount();
     }
 
     /**
-     * [assistHandle 操作区]
+     * [assistHandle 全 大 小 奇 偶 清除 操作区]
      * @param  {[type]} e [description]
      * @return {[type]}   [description]
      */
     assistHandle(e){
-        e.preventDefault();
-        let self=this;
-        let $cur=$(e.currentTarget);
+        e.preventDefault();//取消事件的默认动作
+        let self=this;//指代本作用域的对象
+        let $cur=$(e.currentTarget);//currentTarget 事件属性返回其监听器触发事件的节点
         let index=$cur.index();
-        $('.boll-list .btn-boll').removeClass('btn-boll-active');
-        if(index===0){
+        $('.boll-list .btn-boll').removeClass('btn-boll-active');//清除
+        if(index===0){//全选
             $('.boll-list .btn-boll').addClass('btn-boll-active');
         }
-        if(index===1){
+        if(index===1){//大
             $('.boll-list .btn-boll').each(function(i,t){
                 if(t.textContent-5>0){
                     $(t).addClass('btn-boll-active')
                 }
             })
         }
-        if(index===2){
+        if(index===2){//小
             $('.boll-list .btn-boll').each(function(i,t){
                 if(t.textContent-6<0){
                     $(t).addClass('btn-boll-active')
                 }
             })
         }
-        if(index===3){
+        if(index===3){//奇
             $('.boll-list .btn-boll').each(function(i,t){
                 if(t.textContent%2==1){
                     $(t).addClass('btn-boll-active')
                 }
             })
         }
-        if(index===4){
+        if(index===4){//偶
             $('.boll-list .btn-boll').each(function(i,t){
                 if(t.textContent%2==0){
                     $(t).addClass('btn-boll-active')
@@ -156,15 +159,15 @@ class Base{
      * @return {[type]} [description]
      */
     getName(){
-        return this.name
+        return this.name;
     }
 
     /**
      * [addCode 添加号码]
      */
     addCode(){
-        let self=this;
-        let $active=$('.boll-list .btn-boll-active').text().match(/\d{2}/g);
+        let self=this;//指代本作用域的对象
+        let $active=$('.boll-list .btn-boll-active').text().match(/\d{2}/g);//对获取的值进行2个字符一个字符串数组
         let active=$active?$active.length:0;
         let count=self.computeCount(active,self.cur_play);
         if(count){
@@ -200,8 +203,8 @@ class Base{
         let count=self.computeCount(active,self.cur_play);
         let range=self.computeBonus(active,self.cur_play);
         let money=count*2;
-        let win1=range[0]-money;
-        let win2=range[1]-money;
+        let win1=range[0]-money;//亏损
+        let win2=range[1]-money;//盈利
         let tpl;
         let c1=(win1<0&&win2<0)?Math.abs(win1):win1;
         let c2=(win1<0&&win2<0)?Math.abs(win2):win2;
@@ -244,7 +247,7 @@ class Base{
      */
     getRandom(num){
         let arr=[],index;
-        let number=Array.from(this.number);
+        let number=Array.from(this.number);//Array.from()方法可以将Set结构转换为数组Array.from(new Set(array))
         while(num--){
             index=Number.parseInt(Math.random()*number.length);
             arr.push(number[index]);
@@ -273,5 +276,6 @@ class Base{
     }
 
 }
-
+//this的指向，是在函数被调用的时候确定的，也就是执行上下文被创建时确定的。
+//通过调用函数的 call() 或 apply() 方法可动态的改变 this 的指向。
 export default Base
